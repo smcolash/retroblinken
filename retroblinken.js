17592dbefb3d57392e3d1cfd1751fb723fdf6560
Scott McOlash <s.mcolash@ieee.org>
2021-04-23 00:23:19 +0100

    checkpoint

--- retroblinken.js
+++ retroblinken.js
@@ -4,6 +4,7 @@ class PanelControl {
         this._enabled = enabled;
         this._value = value;
         this._mask = mask;
+        this._changed = false;
     }
 
     //
@@ -12,18 +13,23 @@ class PanelControl {
     set value (x) {
         this._value = x & this._mask;
         this.display ();
+        this._changed = false;
     }
 
     get value () {
         return (this._value);
     }
 
-    enable (state) {
+    enable (state = true) {
         this._enabled = state;
     }
 
     enabled () {
         return (this._enabled);
+    }
+
+    changed () {
+        return (this._changed);
     }
 
     //
@@ -52,6 +58,9 @@ class PanelControl {
         let self = this;
         let temp = self.value;
 
+        console.log (this);
+
+        self._changed = false;
         $('.bit .toggle input[data-name="' + this._name + '"]').each (function (index, element) {
             let input = $(element);
             let data = input.data ();
@@ -72,9 +81,13 @@ class PanelControl {
                     temp = temp & (self._mask ^ (2 ** data.bit));
                 }
             }
+
+            self._changed = true;
         });
 
         self.value = temp;
+
+        return (self._changed);
     }
 }
 
@@ -82,6 +95,7 @@ window.onload = function () {
     //
     // create global system registers
     //
+    var running = false;
     var global = {};
     global['address'] = new PanelControl ('address', false, 0x0000, 0xffff);
     global['data'] = new PanelControl ('data', false, 0x00, 0xff);
@@ -92,11 +106,18 @@ window.onload = function () {
     // initialize the system memory
     //
     var memory = new Array (2**16);
+
+    // fill the memory with chaotic but deterministic values
+    let seed = 8675309;
     for (let loop = 0; loop < 2**16; loop++) {
-        //memory[loop] = loop & 0xff;
-        memory[loop] = Math.trunc (Math.random () * 255);
+        seed = seed * 16807 % 2147483647;
+        memory[loop] = seed & 0xff;
     }
 
+    //
+    // reset the system
+    //
+    running = false;
     global['address'].value = 0;
     global['data'].value = 0;
 
@@ -120,6 +141,10 @@ window.onload = function () {
             return;
         }
 
+        if (!running) {
+            return;
+        }
+
         global['address'].value++;
         global['data'].value = memory[global['address'].value];
 
@@ -127,10 +152,12 @@ window.onload = function () {
             global['address'].value = Math.trunc ((Math.random () * 256)) * 256;
         }
 
+        let tick = 250;
+        //tick = 50; // add turbo mode?
         if ($('#run_stop').is (':checked')) {
             setTimeout (function () {
                 execute (g, m);
-            }, 250);
+            }, tick);
         }
     }
 
@@ -140,23 +167,96 @@ window.onload = function () {
     $('#power').on ('change', function () {
         let power = $(this);
 
-        for (let item in global) {
-            global[item].enable (power.is (':checked'));
-        }
-
+        global['control'].enable (true);
         global['power'].enable (true);
 
         if (!power.is (':checked')) {
+            for (let item in global) {
+                global[item].enable (false);
+            }
+
             global['power'].value = 0;
+            running = false;
+
             global['address'].value = 0;
             global['data'].value = 0;
+            global['control'].value = 0;
+            running = false;
         }
         else {
             global['power'].value = 1;
-            global['address'].value = 0;
-            global['address'].update ();
-            global['data'].update ();
+            running = false;
+
+            global['address'].enable ()
+            global['address'].update ()
+            //global['address'].value = 0;
+
+            global['data'].enable ()
+            global['data'].update ()
+            global['data'].value = memory[global['address'].value];
+
+            global['control'].update ();
+
         }
+    });
+
+    //
+    // handle a request to examine and step one address
+    //
+    // FIXME
+    $('#examine_next').on ('change', function () {
+        if (global['power'].value == 0) {
+            return;
+        }
+
+        if (running) {
+            return;
+        }
+
+        let input = $(this);
+        if (input.is (':checked')) {
+
+            console.log (global['address']);
+
+            global['address'].enable (true);
+            global['address'].update ();
+            if (global['address'].changed ()) {
+                global['address'].value++;
+            }
+            else {
+
+            }
+
+            global['data'].value = memory[global['address'].value];
+
+            console.log (global['address']);
+
+        }
+    });
+
+    //
+    // handle a request to deposit and step one address
+    //
+    // FIXME
+    $('#deposit_next').on ('change', function () {
+        if (global['power'].value == 0) {
+            return;
+        }
+
+        let input = $(this);
+        if (input.is (':checked')) {
+            global['data'].update ();
+
+            let temp = global['address'].value;
+            global['address'].update ();
+
+            if (global['address'].value != temp) {
+                global['address'].value++;
+            }
+
+            global['data'].value = memory[global['address'].value];
+        }
+
     });
 
     //
@@ -165,7 +265,14 @@ window.onload = function () {
     $('#run_stop').on ('change', function () {
         let input = $(this);
         if (input.is (':checked')) {
+            running = true;
+            global['address'].enable (true)
+            global['address'].update ();
+            global['address'].enable (false)
             execute (global, memory);
+        }
+        else {
+            running = false;
         }
     })
 
@@ -179,7 +286,11 @@ window.onload = function () {
 
         let input = $(this);
         if (input.is (':checked')) {
+            running = true;
             execute (global, memory);
+        }
+        else {
+            running = false;
         }
     });
 
