db474471d99dbad20c55df68774b6d861545e8bf
Scott McOlash <s.mcolash@ieee.org>
2021-04-26 03:20:07 +0100

    adding instructions

--- prototype/retroblinken.js
+++ prototype/retroblinken.js
@@ -169,6 +169,7 @@ class Processor {
     constructor () {
         this.registers = {
             'a': 0,
+            'psw': 0,
             'f': {
                 's': 0,
                 'z': 0,
@@ -189,6 +190,7 @@ class Processor {
         this.memory = new Array (2**16);
         this.io = new Array (2**8);
         this.ticks = 50;
+        this.halted = false;
 
         /*
         this.opcodes = {
@@ -284,6 +286,7 @@ class Processor {
     reset () {
         this.clear ();
         this.registers.sp = 0xffff;
+        this.halted = false;
     }
 
     fetch8 () {
@@ -325,6 +328,15 @@ class Processor {
         return ((high << 8) + low);
     }
 
+    _jump (address) {
+        this.registers.pc = address;
+    }
+
+    _call (address) {
+        this.push16 (this.registers.pc);
+        this._jump (address);
+    }
+
     step () {
         //
         // get the next instruction
@@ -355,6 +367,841 @@ class Processor {
                 let temp = this.fetch16 ();
                 this.push16 (this.registers.pc);
                 this.registers.pc = temp;
+                break;
+        }
+    }
+
+    prototype_step () {
+        //
+        // prevent stepping if halted
+        //
+        if (this.halted) {
+            return;
+        }
+
+        //
+        // get the next instruction
+        //
+        let opcode = this.fetch8 ();
+
+        //
+        // process the instruction
+        //
+        switch (opcode) {
+            case 0x00:
+                // null operation
+                break;
+
+            case 0x01:
+                break;
+
+            case 0x02:
+                break;
+
+            case 0x03:
+                break;
+
+            case 0x04:
+                break;
+
+            case 0x05:
+                break;
+
+            case 0x06:
+                break;
+
+            case 0x07:
+                break;
+
+            case 0x08:
+                break;
+
+            case 0x09:
+                break;
+
+            case 0x0a:
+                break;
+
+            case 0x0b:
+                break;
+
+            case 0x0c:
+                break;
+
+            case 0x0d:
+                break;
+
+            case 0x0e:
+                break;
+
+            case 0x0f:
+                break;
+
+            case 0x10:
+                break;
+
+            case 0x11:
+                break;
+
+            case 0x12:
+                break;
+
+            case 0x13:
+                break;
+
+            case 0x14:
+                break;
+
+            case 0x15:
+                break;
+
+            case 0x16:
+                break;
+
+            case 0x17:
+                break;
+
+            case 0x18:
+                break;
+
+            case 0x19:
+                break;
+
+            case 0x1a:
+                break;
+
+            case 0x1b:
+                break;
+
+            case 0x1c:
+                break;
+
+            case 0x1d:
+                break;
+
+            case 0x1e:
+                break;
+
+            case 0x1f:
+                break;
+
+            case 0x20:
+                break;
+
+            case 0x21:
+                break;
+
+            case 0x22:
+                break;
+
+            case 0x23:
+                break;
+
+            case 0x24:
+                break;
+
+            case 0x25:
+                break;
+
+            case 0x26:
+                break;
+
+            case 0x27:
+                break;
+
+            case 0x28:
+                break;
+
+            case 0x29:
+                break;
+
+            case 0x2a:
+                break;
+
+            case 0x2b:
+                break;
+
+            case 0x2c:
+                break;
+
+            case 0x2d:
+                break;
+
+            case 0x2e:
+                break;
+
+            case 0x2f:
+                break;
+
+            case 0x30:
+                break;
+
+            case 0x31:
+                // set the stack pointer
+                this.registers.sp = this.fetch16 ();
+                break;
+
+            case 0x32:
+                break;
+
+            case 0x33:
+                break;
+
+            case 0x34:
+                break;
+
+            case 0x35:
+                break;
+
+            case 0x36:
+                break;
+
+            case 0x37:
+                break;
+
+            case 0x38:
+                break;
+
+            case 0x39:
+                break;
+
+            case 0x3a:
+                break;
+
+            case 0x3b:
+                break;
+
+            case 0x3c:
+                break;
+
+            case 0x3d:
+                break;
+
+            case 0x3e:
+                break;
+
+            case 0x3f:
+                break;
+
+            case 0x40:
+                break;
+
+            case 0x41:
+                break;
+
+            case 0x42:
+                break;
+
+            case 0x43:
+                break;
+
+            case 0x44:
+                break;
+
+            case 0x45:
+                break;
+
+            case 0x46:
+                break;
+
+            case 0x47:
+                break;
+
+            case 0x48:
+                break;
+
+            case 0x49:
+                break;
+
+            case 0x4a:
+                break;
+
+            case 0x4b:
+                break;
+
+            case 0x4c:
+                break;
+
+            case 0x4d:
+                break;
+
+            case 0x4e:
+                break;
+
+            case 0x4f:
+                break;
+
+            case 0x50:
+                break;
+
+            case 0x51:
+                break;
+
+            case 0x52:
+                break;
+
+            case 0x53:
+                break;
+
+            case 0x54:
+                break;
+
+            case 0x55:
+                break;
+
+            case 0x56:
+                break;
+
+            case 0x57:
+                break;
+
+            case 0x58:
+                break;
+
+            case 0x59:
+                break;
+
+            case 0x5a:
+                break;
+
+            case 0x5b:
+                break;
+
+            case 0x5c:
+                break;
+
+            case 0x5d:
+                break;
+
+            case 0x5e:
+                break;
+
+            case 0x5f:
+                break;
+
+            case 0x60:
+                break;
+
+            case 0x61:
+                break;
+
+            case 0x62:
+                break;
+
+            case 0x63:
+                break;
+
+            case 0x64:
+                break;
+
+            case 0x65:
+                break;
+
+            case 0x66:
+                break;
+
+            case 0x67:
+                break;
+
+            case 0x68:
+                break;
+
+            case 0x69:
+                break;
+
+            case 0x6a:
+                break;
+
+            case 0x6b:
+                break;
+
+            case 0x6c:
+                break;
+
+            case 0x6d:
+                break;
+
+            case 0x6e:
+                break;
+
+            case 0x6f:
+                break;
+
+            case 0x70:
+                break;
+
+            case 0x71:
+                break;
+
+            case 0x72:
+                break;
+
+            case 0x73:
+                break;
+
+            case 0x74:
+                break;
+
+            case 0x75:
+                break;
+
+            case 0x76:
+                // hlt
+                this.halted = true;
+                break;
+
+            case 0x77:
+                break;
+
+            case 0x78:
+                break;
+
+            case 0x79:
+                break;
+
+            case 0x7a:
+                break;
+
+            case 0x7b:
+                break;
+
+            case 0x7c:
+                break;
+
+            case 0x7d:
+                break;
+
+            case 0x7e:
+                break;
+
+            case 0x7f:
+                break;
+
+            case 0x80:
+                break;
+
+            case 0x81:
+                break;
+
+            case 0x82:
+                break;
+
+            case 0x83:
+                break;
+
+            case 0x84:
+                break;
+
+            case 0x85:
+                break;
+
+            case 0x86:
+                break;
+
+            case 0x87:
+                break;
+
+            case 0x88:
+                break;
+
+            case 0x89:
+                break;
+
+            case 0x8a:
+                break;
+
+            case 0x8b:
+                break;
+
+            case 0x8c:
+                break;
+
+            case 0x8d:
+                break;
+
+            case 0x8e:
+                break;
+
+            case 0x8f:
+                break;
+
+            case 0x90:
+                break;
+
+            case 0x91:
+                break;
+
+            case 0x92:
+                break;
+
+            case 0x93:
+                break;
+
+            case 0x94:
+                break;
+
+            case 0x95:
+                break;
+
+            case 0x96:
+                break;
+
+            case 0x97:
+                break;
+
+            case 0x98:
+                break;
+
+            case 0x99:
+                break;
+
+            case 0x9a:
+                break;
+
+            case 0x9b:
+                break;
+
+            case 0x9c:
+                break;
+
+            case 0x9d:
+                break;
+
+            case 0x9e:
+                break;
+
+            case 0x9f:
+                break;
+
+            case 0xa0:
+                break;
+
+            case 0xa1:
+                break;
+
+            case 0xa2:
+                break;
+
+            case 0xa3:
+                break;
+
+            case 0xa4:
+                break;
+
+            case 0xa5:
+                break;
+
+            case 0xa6:
+                break;
+
+            case 0xa7:
+                break;
+
+            case 0xa8:
+                break;
+
+            case 0xa9:
+                break;
+
+            case 0xaa:
+                break;
+
+            case 0xab:
+                break;
+
+            case 0xac:
+                break;
+
+            case 0xad:
+                break;
+
+            case 0xae:
+                break;
+
+            case 0xaf:
+                break;
+
+            case 0xb0:
+                break;
+
+            case 0xb1:
+                break;
+
+            case 0xb2:
+                break;
+
+            case 0xb3:
+                break;
+
+            case 0xb4:
+                break;
+
+            case 0xb5:
+                break;
+
+            case 0xb6:
+                break;
+
+            case 0xb7:
+                break;
+
+            case 0xb8:
+                break;
+
+            case 0xb9:
+                break;
+
+            case 0xba:
+                break;
+
+            case 0xbb:
+                break;
+
+            case 0xbc:
+                break;
+
+            case 0xbd:
+                break;
+
+            case 0xbe:
+                break;
+
+            case 0xbf:
+                break;
+
+            case 0xc0:
+                break;
+
+            case 0xc1:
+                // pop B
+                this.registers.c = this.pop8 ();
+                this.registers.b = this.pop8 ();
+                break;
+
+            case 0xc2:
+                break;
+
+            case 0xc3:
+                // jump to address
+                this.registers.pc = this.fetch16 ();
+                break;
+
+            case 0xc4:
+                break;
+
+            case 0xc5:
+                // push B
+                this.push8 (this.register.b);
+                this.push8 (this.register.c);
+                break;
+
+            case 0xc6:
+                break;
+
+            case 0xc7:
+                // rst 0
+                this._call (0x0000);
+                break;
+
+            case 0xc8:
+                break;
+
+            case 0xc9:
+                // return from subroutine
+                this.registers.pc = this.pop16 ();
+                break;
+
+            case 0xca:
+                break;
+
+            case 0xcb:
+                break;
+
+            case 0xcc:
+                break;
+
+            case 0xcd:
+                // call subroutine at address
+                this._call (this.fetch16 ());
+                break;
+
+            case 0xce:
+                break;
+
+            case 0xcf:
+                // rst 1
+                this._call (0x0008);
+                break;
+
+            case 0xd0:
+                break;
+
+            case 0xd1:
+                // pop D
+                this.registers.e = this.pop8 ();
+                this.registers.d = this.pop8 ();
+                break;
+
+            case 0xd2:
+                break;
+
+            case 0xd3:
+                break;
+
+            case 0xd4:
+                break;
+
+            case 0xd5:
+                // push D
+                this.push8 (this.register.d);
+                this.push8 (this.register.e);
+                break;
+
+            case 0xd7:
+                // rst 2
+                this._call (0x0010);
+                break;
+
+            case 0xd8:
+                break;
+
+            case 0xd9:
+                break;
+
+            case 0xda:
+                break;
+
+            case 0xdb:
+                break;
+
+            case 0xdc:
+                break;
+
+            case 0xdd:
+                break;
+
+            case 0xde:
+                break;
+
+            case 0xdf:
+                // rst 3
+                this._call (0x0018);
+                break;
+
+            case 0xe0:
+                break;
+
+            case 0xe1:
+                // pop H
+                this.registers.l = this.pop8 ();
+                this.registers.h = this.pop8 ();
+                break;
+
+            case 0xe2:
+                break;
+
+            case 0xe3:
+                break;
+
+            case 0xe4:
+                break;
+
+            case 0xe5:
+                // push H
+                this.push8 (this.register.h);
+                this.push8 (this.register.l);
+                break;
+
+            case 0xe6:
+                break;
+
+            case 0xe7:
+                // rst 4
+                this._call (0x0020);
+                break;
+
+            case 0xe8:
+                break;
+
+            case 0xe9:
+                break;
+
+            case 0xea:
+                break;
+
+            case 0xeb:
+                break;
+
+            case 0xec:
+                break;
+
+            case 0xed:
+                break;
+
+            case 0xee:
+                break;
+
+            case 0xef:
+                // rst 5
+                this._call (0x0028);
+                break;
+
+            case 0xf0:
+                break;
+
+            case 0xf1:
+                // pop PSW
+                this.registers.psw = this.pop8 ();
+                this.registers.a = this.pop8 ();
+                break;
+
+            case 0xf2:
+                break;
+
+            case 0xf3:
+                break;
+
+            case 0xf4:
+                break;
+
+            case 0xf5:
+                // push PSW
+                this.push8 (this.register.a);
+                this.push8 (this.register.psw);
+                break;
+
+            case 0xf6:
+                break;
+
+            case 0xf7:
+                // rst 6
+                this._call (0x0030);
+                break;
+
+            case 0xf8:
+                break;
+
+            case 0xf9:
+                break;
+
+            case 0xfa:
+                break;
+
+            case 0xfb:
+                break;
+
+            case 0xfc:
+                break;
+
+            case 0xfd:
+                break;
+
+            case 0xfe:
+                break;
+
+            case 0xff:
+                // rst 7
+                this._call (0x0038);
                 break;
         }
     }
